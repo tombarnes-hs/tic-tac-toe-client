116553a903eeaeaf46b893bb4f2a4bf86971e03b
Tom Guncik <tom.guncik@gmail.com>
2018-09-26 12:13:47 -0400

    Added working curl script file
    Sign-up script written, html edited

---
+++ curl-scripts/sign-up.sh
@@ -0,0 +1,15 @@
+# sh curl-scripts/sign-up.sh
+
+curl "https://tic-tac-toe-wdi.herokuapp.com/sign-up" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'",
+      "password_confirmation": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
