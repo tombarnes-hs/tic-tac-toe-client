f164e5402ae7d0a158b985ee0af289057c6c56e6
Tom Guncik <tom.guncik@gmail.com>
2018-09-28 10:39:33 -0400

    Add Game end logic
    jQuery .off method stops boxes from having event listener when game end
    condition is met

--- assets/scripts/gameJS/game.js
+++ assets/scripts/gameJS/game.js
@@ -29,34 +29,41 @@ const checkForVictory = function () {
   // after a user.play being successful, invoke check for victory condition function
   // victory is [0], [1], [3] having the same values
   if (gameBoard[0] === gameBoard[1] && gameBoard[1] === gameBoard[2] && gameBoard[0] !== '') {
+    $('.box').off()
     console.log('Row 1 victory')
   // victory is [4], [5], [6] having the same values
   } else if (gameBoard[3] === gameBoard[4] && gameBoard[4] === gameBoard[5] && gameBoard[3] !== '') {
+    $('.box').off()
     console.log('Row 2 victory')
   // victory is [7], [8], [9] having the same values
   } else if (gameBoard[6] === gameBoard[7] && gameBoard[7] === gameBoard[8] && gameBoard[6] !== '') {
+    $('.box').off()
     console.log('Row 3 victory')
   // victory is [0], [3], [6] having the same values
   } else if (gameBoard[0] === gameBoard[3] && gameBoard[3] === gameBoard[6] && gameBoard[0] !== '') {
+    $('.box').off()
     console.log('Column 1 victory')
   // victory is [1], [4], [7] having the same values
   } else if (gameBoard[1] === gameBoard[4] && gameBoard[4] === gameBoard[5] && gameBoard[1] !== '') {
+    $('.box').off()
     console.log('Column 2 victory')
   // victory is [2], [5], [8] having the same values
   } else if (gameBoard[2] === gameBoard[5] && gameBoard[5] === gameBoard[8] && gameBoard[2] !== '') {
+    $('.box').off()
     console.log('Column 3 victory')
   // victory is [0], [4], [8] having the same values
   } else if (gameBoard[0] === gameBoard[4] && gameBoard[4] === gameBoard[8] && gameBoard[0] !== '') {
+    $('.box').off()
     console.log('Diagonal l-r victory')
   // victory is [2], [4], [6] having the same values
   } else if (gameBoard[2] === gameBoard[4] && gameBoard[4] === gameBoard[6] && gameBoard[2] !== '') {
+    $('.box').off()
     console.log('Diagonal r-l victory')
   // else: user may play again
   } else {
     if (playCount === 9) {
+      $('.box').off()
       console.log('game ends in tie')
-    } else {
-      console.log('User plays again')
     }
   }
 }
